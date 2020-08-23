bb47f1f1efaded050d57bc1b0113860bc7d5e4d9
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-23 02:37:52 -0400

    Added filter only for ULR's

--- sniff.py
+++ sniff.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import scapy.all as scapy
 from scapy.layers import http
+
 def psniff(interface):
     scapy.sniff(iface=interface, store=False, prn=see_packets)
-
 def see_packets(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+        URL = "http://www." + packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        print(URL)
 
 psniff("eth0")
