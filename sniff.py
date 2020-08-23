148e20fc82716da8a46c72317062b2f3da073a5b
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-23 02:00:45 -0400

    simple HTTP filter added and sniffed (BPF don't support port 80)

--- sniff.py
+++ sniff.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import scapy.all as scapy
-
+from scapy.layers import http
 def psniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=see_packets, filter="tcp")    #"udp", "port 21", "ftp" etc..
+    scapy.sniff(iface=interface, store=False, prn=see_packets)
 
 def see_packets(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
 
 psniff("eth0")
