99010a329aa6f385b414129816278b4b98e90602
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-23 01:53:50 -0400

    Added filters for capturing from specific port

--- sniff.py
+++ sniff.py
@@ -2,7 +2,7 @@
 import scapy.all as scapy
 
 def psniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=see_packets)
+    scapy.sniff(iface=interface, store=False, prn=see_packets, filter="tcp")    #"udp", "port 21", "ftp" etc..
 
 def see_packets(packet):
     print(packet)
